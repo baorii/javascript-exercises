9c2cc2a53038338273f594e311d3c2a9b7e3ba1a
baori <john.guibao127@gmail.com>
2023-01-11 21:58:44 +0000

    Completed task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num1, num2, num3, num4) {
+    let len = array.length;
+    let newArray = [];
+    let j = 0;
+    for (i=0;i<len;i++) {
+        if (array[i] !== num1) {
+            if(array[i] != num2) {
+                if(array[i] != num3) {
+                    if(array[i] != num4) {
+                        newArray.push(array[i]);
+                    }
+                }
+            }
+        }    
+        
+    }
+    return newArray
 
 };
 
