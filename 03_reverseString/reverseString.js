3d516b526c89037efc5830ef5cf179322cd8c7ef
baori <john.guibao127@gmail.com>
2023-01-11 21:20:00 +0000

    Completed Task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let total = "";
+    len = text.length;
+    for (i = len; i>=0; i--) {
+        total += (String)(text.charAt(i));
+    }
+    return total;
 };
 
 // Do not edit below this line
