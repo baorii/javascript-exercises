aea9447a559dd3d84315675ed45e2f59ac20fd60
baori <john.guibao127@gmail.com>
2023-01-11 21:08:12 +0000

    Completed Task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    let total="";
+    if (num <= 0) {
+        total += "ERROR";
+    }
 
+    for (num>0; num--;) {
+        total += text
+    }
+
+    return total
 };
 
 // Do not edit below this line
